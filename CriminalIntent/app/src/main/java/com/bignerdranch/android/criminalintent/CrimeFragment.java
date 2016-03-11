1089f19b81deab73a87e45b53050a57d11f25a2a
Oliver Nelson <onelson@uievolution.com>
2016-03-10 17:37:39 -0800

    Cloudizing all android exercise work from text into public repository.

---
+++ CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/CrimeFragment.java
@@ -0,0 +1,84 @@
+package com.bignerdranch.android.criminalintent;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+
+import java.text.DateFormat;
+import java.text.FieldPosition;
+import java.text.ParsePosition;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Created by olivernelson on 12/1/15.
+ */
+public class CrimeFragment extends Fragment {
+
+    // widgets
+    private Crime mCrime;
+    private EditText mTitleField;
+    private Button mDateButton;
+    private CheckBox mSolvedCheckBox;
+
+    // consts
+    private static final String CRIME_DATE_FORMAT = "EEEE, MMM d yyyy";
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mCrime = new Crime();
+
+    }
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_crime, container, false);
+
+        mTitleField = (EditText) v.findViewById(R.id.crime_title);
+        mTitleField.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                // This space intentionally left blank
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                mCrime.setTitle(s.toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                // This one too
+            }
+        });
+
+        mDateButton = (Button) v.findViewById(R.id.crime_date);
+
+
+
+        SimpleDateFormat sdf = new SimpleDateFormat(CRIME_DATE_FORMAT);
+        mDateButton.setText(sdf.format(mCrime.getDate()).toString());
+        mDateButton.setEnabled(false);
+
+        mSolvedCheckBox = (CheckBox) v.findViewById(R.id.crime_solved);
+        mSolvedCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                mCrime.setSolved(isChecked);
+            }
+        });
+
+        return v;
+
+    }
+
+}
